ed45a3d65d354f7019af1ae5daf8b0454035b5e0
L-Trunks <18742509891@163.com>
2020-12-29 16:49:00 +0800

    优化

--- miniprogram/util/util.js
+++ miniprogram/util/util.js
@@ -21,8 +21,8 @@ export const formatDateTime = function(inputTime, type) {
     return resultMap[type];
 };
 export const randomsort = function(a, b) {
-    return Math.random() > .5 ? -1 : 1;
-    //用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
+    return Math.random() > .5 ? true : false;
+    //随机返回true或false
 }
 
 //日期转时间戳
@@ -150,7 +150,5 @@ export const getSingleArr = (arr)=>{
             result.push(i)
         }
     })
-
-    console.log(result)
     return result
 }
